7deb322d5f0612848d4e77af47a7b35926c627a6
Daniel Li <danli@lcs.on.ca>
2024-11-11 12:22:21 -0500

    Initial Commit

---
+++ Trading Cards/Trading_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_CardsApp.swift
+//  Trading Cards
+//
+//  Created by 李泽宇 on 2024-11-11.
+//
+
+import SwiftUI
+
+@main
+struct Trading_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
