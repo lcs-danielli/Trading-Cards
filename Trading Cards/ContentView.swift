75611d80819c7b1c70acd31c668a8b6743754bae
Daniel Li <danli@lcs.on.ca>
2024-11-12 14:36:49 -0500

    Background struct

--- Trading Cards/ContentView.swift
+++ Trading Cards/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("TFT Image")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .opacity(0.6)
         }
-        .padding()
     }
 }
 
