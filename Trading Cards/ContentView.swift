67e74601bcaf5aefa59f96af5485d918a3a33994
Daniel Li <danli@lcs.on.ca>
2024-11-16 18:28:13 -0500

    Done everything for zstack and vstack inside included

--- Trading Cards/ContentView.swift
+++ Trading Cards/ContentView.swift
@@ -14,16 +14,53 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
-                .opacity(0.6)
+                .opacity(0.4)
             
-            Color.yellow
-                .padding(.horizontal, 270)
+            Image("frame")
+                .resizable()
+                .scaledToFit()
+                .ignoresSafeArea()
+                .frame(width: 670, height: 700)
+            
             VStack{
-                Image("Briar")
+                Text("Briar")
+                    .font(.largeTitle)
+
+                    .fontWeight(.black)
+                Image("Briar true")
                     .resizable()
                     .scaledToFit()
                     .clipShape(Circle())
-                Spacer(minLength: 500)
+                Text("Certain Death")
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .font(.system(.body, design: .serif))
+                    .bold()
+
+                Text("Active")
+                    .font(.subheadline)
+                    .foregroundStyle(.lightblue)
+                    .font(.system(.body, design: .serif))
+                    .bold()
+
+                HStack{
+                    Image("Skill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:100)
+                    Text("Dealing physical damage to enemies. Then enter a frenzy, gaining Attack Speed, 15% Omnivamp, and causing attacks to deal bonus physical damage to the target and adjacent enemies.")
+                        .frame(width:250)
+                        .background(.ultraThinMaterial)
+                        .foregroundStyle(.lightblue)
+                        .font(.system(.body, design: .serif))
+                }.padding()
+                Text("A failed experiment by the Black Rose, Briar's uncontrollable bloodlust required a special pillory to focus her frenzied mind. After years of confinement, this living weapon broke free from her restraints and unleashed herself into the world. Now she's controlled by no one—following only her hunger.")
+                    .frame(width:360)
+                Image("Tft")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                Spacer(minLength: 60)
             }
         }
     }
